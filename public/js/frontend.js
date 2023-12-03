dc3990801fcba831858aca49cb7054ca7e95f47e
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2023-12-02 23:29:01 -0300

    avoids an error message that causes no problems

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -14,26 +14,28 @@ window.addEventListener('load', function () {
     const frontendItems = [];
 
     socket.on('updatePlayers', (allItems) => {
-        frontendItems.splice(0,frontendItems.length);
-        allItems.forEach(i => frontendItems.push(i));
+        frontendItems.splice(0, frontendItems.length);
+        if (allItems)
+            allItems.forEach(i => frontendItems.push(i));
     })
 
-    socket.on('dimensions', (dims) =>{
+    socket.on('dimensions', (dims) => {
         canvas.width = dims.width * devicePixelRatio;
         canvas.height = dims.height * devicePixelRatio;
     })
 
     window.addEventListener('keydown', (event) => {
-        socket.emit('keydown',event.key);
+        socket.emit('keydown', event.key);
     })
     window.addEventListener('keyup', (event) => {
-        socket.emit('keyup',event.key);
+        socket.emit('keyup', event.key);
     })
 
     let lastTime = 0;
+
     function animate(timeStamp) {
         const deltaTime = timeStamp - lastTime;
-        if(deltaTime >= 60 / this.FPS_CAP) {
+        if (deltaTime >= 60 / this.FPS_CAP) {
             lastTime = timeStamp;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             reScale(frontendItems);
@@ -44,27 +46,28 @@ window.addEventListener('load', function () {
 
     animate();
 
-    function reScale(frontendItems){
-        if(devicePixelRatio !== 1)
-        frontendItems.forEach(all => all.forEach(pointCollection => pointCollection.points.forEach(p => {
-            p.x *= devicePixelRatio;
-            p.y *= devicePixelRatio;
-        })));
+    function reScale(frontendItems) {
+        if (devicePixelRatio !== 1)
+            frontendItems.forEach(all => all.forEach(pointCollection => pointCollection.points.forEach(p => {
+                p.x *= devicePixelRatio;
+                p.y *= devicePixelRatio;
+            })));
     }
-    function draw(pointCollection){
+
+    function draw(pointCollection) {
         ctx.fillStyle = pointCollection.fillColor;
-        if(pointCollection.points.length <= 1){
+        if (pointCollection.points.length <= 1) {
             ctx.fillStyle = pointCollection.fillColor;
-            ctx.fillRect(pointCollection.points[0].x,pointCollection.points[0].y,4,4)
+            ctx.fillRect(pointCollection.points[0].x, pointCollection.points[0].y, 4, 4)
             return;
         }
         ctx.strokeStyle = pointCollection.strokeColor;
         ctx.beginPath();
-        ctx.moveTo(pointCollection.points[0].x,pointCollection.points[0].y);
-        pointCollection.points.forEach(p =>{
-            ctx.lineTo(p.x,p.y)
+        ctx.moveTo(pointCollection.points[0].x, pointCollection.points[0].y);
+        pointCollection.points.forEach(p => {
+            ctx.lineTo(p.x, p.y)
         })
-        ctx.lineTo(pointCollection.points[0].x,pointCollection.points[0].y);
+        ctx.lineTo(pointCollection.points[0].x, pointCollection.points[0].y);
         ctx.closePath();
         ctx.fill();
         ctx.stroke();
